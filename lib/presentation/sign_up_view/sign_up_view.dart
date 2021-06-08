d93fc8f6f1437b3b7743da5231a3341f672306b2
sjsilver <stephensilverstein@gmail.com>
2021-06-07 20:48:56 -0700

    firebase login and form
    TODO update TextField with form validation

---
+++ lib/presentation/sign_up_view/sign_up_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:wh40k_crusader/presentation/dumb_widgets/authentication_layout.dart';
+import 'package:wh40k_crusader/presentation/login_view/login_view_model.dart';
+
+class LoginView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<LoginViewModel>.reactive(
+      viewModelBuilder: () => LoginViewModel(),
+      builder: (context, model, child) => Scaffold(
+        body: AuthenticationLayout(
+          busy: model.isBusy,
+          onCreateAccountTapped: () {},
+          title: 'Welcome',
+          subtitile: 'Enter your email address to sign in.',
+          mainButtonTitle: 'SIGN IN',
+          form: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+              ),
+            ],
+          ),
+          onForgotPassword: () {},
+        ),
+      ),
+    );
+  }
+}
