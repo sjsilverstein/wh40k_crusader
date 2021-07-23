f1c529664a1eb4758632896a1949f289fd7ae103
sjsilver <stephensilverstein@gmail.com>
2021-07-23 13:01:04 -0700

    package import renamed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:wh40k_crusader/app/locator.dart';
 import 'package:wh40k_crusader/app/setup_dialog_ui.dart';
 import 'package:wh40k_crusader/presentation/shared/wh40kAppTheme.dart';
 import 'package:wh40k_crusader/routing/router.dart' as projectRouter;
 import 'package:wh40k_crusader/routing/routes.dart';
-
-import 'app/locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
